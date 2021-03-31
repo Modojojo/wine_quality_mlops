4340392d176120f2acc330b187d2cfbf8741e0e8
Modojojo <rmodi243@gmail.com>
2021-04-01 02:11:58 +0530

    setup.py and tox added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="wine quality prediction package",
+    author="Modojojo",
+    packages=find_packages(),
+    liscence="MIT"
+)
